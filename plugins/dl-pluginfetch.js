c8891fd8d10dedd8d02bb492885cb68d990f936a
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-01-16 22:36:50 +0200

    Create dl-pluginfetch.js

---
+++ plugins/dl-pluginfetch.js
@@ -0,0 +1,32 @@
+
+const config = require('../config');
+const fs = require('fs');
+const axios = require('axios');
+const { cmd } = require('../command');
+
+cmd({
+  pattern: "fetch",
+  react: "🗂️",
+  desc: "Download a specific file from the GitHub repository",
+  category: "system",
+  use: '.fetch <filename>',
+  filename: __filename
+}, async (conn, mek, m, { from, reply, match }) => {
+  try {
+    const filename = match[1];
+    if (!filename) {
+      return reply("Please provide a filename");
+    }
+
+    const repoUrl = 'https://raw.githubusercontent.com/mrfrank-ofc/SUBZERO-MD/main/plugins/';
+    const fileUrl = repoUrl + filename;
+
+    const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
+    const buffer = Buffer.from(response.data, 'utf-8');
+
+    await conn.sendMessage(from, { document: { url: fileUrl }, mimetype: 'application/octet-stream', filename }, { quoted: mek });
+  } catch (error) {
+    console.error(error);
+    reply(`Error fetching file: ${error.message}`);
+  }
+});
