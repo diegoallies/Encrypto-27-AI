63833212ebe705f2ffa3c7016a913cd0f3a06ecb
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-01-19 22:40:32 +0200

    Update tools-jsobf.js

--- plugins/tools-jsobf.js
+++ plugins/tools-jsobf.js
@@ -3,6 +3,45 @@ const config = require('../config');
 const { cmd, commands } = require('../command');
 const { fetchJson } = require('../lib/functions');
 
+
+cmd({
+pattern: 'base64',
+desc: 'Convert text to base64.',
+category: 'tools',
+react: '❣️',
+filename: __filename
+}, async (conn, mek, m, { from, text, reply }) => {
+const api = 'https://api.giftedtech.web.id/api/tools/ebase?apikey=gifted&query=';
+try {
+const res = await fetch(api + encodeURIComponent(text));
+const json = await res.json();
+reply(`Base64: ${json.result}`);
+} catch (error) {
+reply(`Error: ${error.message}`);
+}
+});
+
+
+
+cmd({
+pattern: 'debase64',
+desc: 'Decode base64 to normal text.',
+category: 'tools',
+react: '💞',
+filename: __filename
+}, async (conn, mek, m, { from, text, reply }) => {
+const api = 'https://api.giftedtech.web.id/api/tools/dbase?apikey=gifted&query=';
+try {
+const res = await fetch(api + encodeURIComponent(text));
+const json = await res.json();
+reply(`Decoded: ${json.result}`);
+} catch (error) {
+reply(`Error: ${error.message}`);
+}
+});
+
+
+
 /*cmd({
   pattern: 'obfuscate',
   alias: ['obf'],
@@ -61,19 +100,3 @@ const { fetchJson } = require('../lib/functions');
   }
 });
 */
-cmd({
-pattern: 'base64',
-desc: 'Convert text to base64.',
-category: 'tools',
-react: '❣️',
-filename: __filename
-}, async (conn, mek, m, { from, text, reply }) => {
-const api = 'https://api.giftedtech.web.id/api/tools/ebase?apikey=gifted&query=';
-try {
-const res = await fetch(api + encodeURIComponent(text));
-const json = await res.json();
-reply(`Base64: ${json.result}`);
-} catch (error) {
-reply(`Error: ${error.message}`);
-}
-});
