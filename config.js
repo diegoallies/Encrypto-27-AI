ffefd4a5180e4aa8c8054f9e762700a8b8f939a5
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-01-21 09:22:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your subzero md session id here",
+SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~TmZAmaiR#elUx_s2pstWHUO9FMwHBBngcCLg5teD8BwX68Npdlmo",
  
 PREFIX: process.env.PREFIX || ".",
 // Enter Your Desired Prefix
